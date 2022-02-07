d68948830c1221af0c1bb1000797c52ab1eeae22
Sarvin <sarvin.to@gmail.com>
2022-02-07 21:33:37 +0330

    file uplade Database = filepond

--- models/book.js
+++ models/book.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const path = require("path");
-const coverimageBasePath = "uploads/bookCovers";
+// const path = require("path");
+// const coverimageBasePath = "uploads/bookCovers";
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -28,16 +28,22 @@ const bookSchema = new mongoose.Schema({
     type: Date,
     required: true,
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true,
+  },
+  coverImageType: {
     type: String,
     required: true,
   },
 });
 bookSchema.virtual("coverImagePath").get(function () {
-  if (this.coverImageName != null) {
-    return path.join("/", coverimageBasePath, this.coverImageName);
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString("base64")}`;
   }
 });
 
 module.exports = mongoose.model("Book", bookSchema);
-module.exports.coverimageBasePath = coverimageBasePath;
+// module.exports.coverimageBasePath = coverimageBasePath;
--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const multer = require("multer");
-const path = require("path");
+// const fs = require("fs");
+// const multer = require("multer");
+// const path = require("path");
 
 const Book = require("../models/book");
 const Author = require("../models/author");
 
-const uploadPath = path.join("public", Book.coverimageBasePath);
+//const uploadPath = path.join("public", Book.coverimageBasePath);
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, callback) =>
-    callback(null, imageMimeTypes.includes(file.mimetype)),
-});
+// const upload = multer({
+//   dest: uploadPath,
+//   fileFilter: (req, file, callback) =>
+//     callback(null, imageMimeTypes.includes(file.mimetype)),
+// });
 // All Books Route
 router.get("/", async (req, res) => {
   let query = Book.find();
@@ -43,34 +43,35 @@ router.get("/new", async (req, res) => {
 
 // Create Book Route
 
-router.post("/", upload.single("cover"), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null;
+//router.post("/", upload.single("cover"), async (req, res) => {
+router.post("/", async (req, res) => {
+  // const fileName = req.file != null ? req.file.filename : null;
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
+    // coverImageName: fileName,
     description: req.body.description,
   });
-
+  saveCover(book, req.body.cover);
   try {
     const newBook = await book.save();
     //res.redirect(`books/${newBook.id}`)
     res.redirect("books");
   } catch {
-    if (book.coverImageName != null) {
-      removerBookCover(book.coverImageName);
-    }
+    // if (book.coverImageName != null) {
+    //   removerBookCover(book.coverImageName);
+    // }
     renderNewPage(res, book, true);
   }
 });
 
-function removerBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), (err) => {
-    if (err) console.error(err);
-  });
-}
+// function removerBookCover(fileName) {
+//   fs.unlink(path.join(uploadPath, fileName), (err) => {
+//     if (err) console.error(err);
+//   });
+// }
 async function renderNewPage(res, book, hasError = false) {
   try {
     const authors = await Author.find({});
@@ -83,4 +84,12 @@ async function renderNewPage(res, book, hasError = false) {
   }
 }
 
+function saveCover(book, encodedCover) {
+  if (encodedCover == null) return;
+  const cover = JSON.parse(encodedCover);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
+  }
+}
 module.exports = router;
