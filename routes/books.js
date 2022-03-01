51e4fbc5abecba2d09e663a8ae136fa798e94b27
Sarvin <sarvin.to@gmail.com>
2022-03-01 21:17:56 +0330

    addedsome general css styles

--- routes/books.js
+++ routes/books.js
@@ -115,7 +115,7 @@ router.put("/:id", async (req, res) => {
     if (book != null) {
       renderEditPage(res, book, true);
     } else {
-      redirect("/");
+      res.redirect("/");
     }
   }
 });
